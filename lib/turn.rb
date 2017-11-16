dc96e2186127d99beb6014d5b2bb01a7a9bf51cd
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 16:29:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,21 @@ def display_board(board)
   puts "#{board[6] | #{board[7] | #{board[8]"
 
 end
+
+def valid_move?(board , index)
+  if position_taken?(board , index) || index.between?(0 , 8)
+    true
+  else
+    false 
+  end
+end
+
+def position_taken?(board , index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    true
+  elsif board[index] == "X" || board[index] == "O"
+    false 
+ end
+end
+
+def
