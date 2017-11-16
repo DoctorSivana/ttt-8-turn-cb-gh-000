6d783875ef286e122b2e8803200190380f7d7c08
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 16:50:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def turn(board)
   input = gets.stripe
   index = input_to_index(input)
   if valid_move?(board , index)
-    move(board, index, choice = "X")
+    move(board, index, "X")
   else
     turn(board)
   end
